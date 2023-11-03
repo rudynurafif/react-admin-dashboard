cc8f6be8650df911f3b6788106a08a09a2628f9a
rudynurafif <rudynurafif@gmail.com>
2023-11-04 02:17:50 +0700

    Create dashboard layout

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>ranadmin</span>
+      <span>Dashboard by &copy; Rudy Asa Nurafif</span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import './navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="logo">
+        <img src="logo.svg" alt="ran-admin" />
+        <span>ranadmin</span>
+      </div>
+      <div className="icons">
+        <img src="search.svg" alt="" className="icon" />
+        <img src="app.svg" alt="" className="icon" />
+        <img src="expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://rudynurafif.com/assets/img/rudy.jpg" alt="" />
+          <span>Rudy</span>
+        </div>
+        <img src="settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
